6cb51db072771c5285495a5e8ba032e01d92755f
Basant-Adel <basantadel99@gmail.com>
2024-03-14 10:39:26 +0000

    fix error :D

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import startServer from './libs/boot';
 import injectRoutes from './routes';
 import injectMiddlewares from './libs/middlewares';
+
 const server = express();
 
 injectMiddlewares(server);
